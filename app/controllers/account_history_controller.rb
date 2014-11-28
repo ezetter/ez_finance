bd75fa6a3839fad23c79461a6173eb9dfdf64671
Eric Zetterbaum <ezetter@gmail.com>
2014-11-28 14:17:57 -0800

    Fixed up history controller.

--- app/controllers/account_history_controller.rb
+++ app/controllers/account_history_controller.rb
@@ -1,14 +1,11 @@
 class AccountHistoryController < ApplicationController
 
   def form
-    @accounts = [['All accounts', 0]]
-    @accounts += Account.all.sort.map {|acc| [acc.name, acc.id]}
-    @account_types = [['All accounts types', 0]]
-    @account_types += AccountType.all.sort_by { |at| at.description }
-                          .map {|at| [at.description, at.id]}
-    @account_owners = [['All account owners', 0]]
-    @account_owners += AccountOwner.all.sort_by { |at| at.description }
-                          .map {|at| [at.description, at.id]}
+    @accounts = Account.all.sort.map { |acc| [acc.name, acc.id] }
+    @account_types = AccountType.all.sort_by { |at| at.description }
+                         .map { |at| [at.description, at.id] }
+    @account_owners = AccountOwner.all.sort_by { |at| at.description }
+                          .map { |at| [at.description, at.id] }
   end
 
   def view
@@ -21,11 +18,14 @@ class AccountHistoryController < ApplicationController
       flash[:error] = "Invalid number entered."
       redirect_to action: 'form'
     end
-    if params[:account_id] == '0'
+    if params[:account_id].empty? && params[:account_type_id].empty? && params[:account_owner_id].empty?
       @displayed = "History for all accounts:"
     else
-      account = Account.find(params[:account_id])
-      @displayed = "History for account #{account.name}:"
+      filters = []
+      filters << "#{Account.find(params[:account_id]).name}" unless params[:account_id].empty?
+      filters << "type #{AccountType.find(params[:account_type_id]).description}" unless params[:account_type_id].empty?
+      filters << "owner #{AccountOwner.find(params[:account_owner_id]).name}" unless params[:account_owner_id].empty?
+      @displayed = "History for account #{filters.join(' and ')}:"
     end
     get_history(intervals, interval_size)
   end
@@ -35,15 +35,15 @@ class AccountHistoryController < ApplicationController
   def get_history(intervals, interval_size)
     select_string = 'date_changed IS NOT NULL'
     query_params = []
-    unless params[:account_id] == '0'
+    unless params[:account_id].empty?
       select_string += ' AND account_id = ?'
       query_params << params[:account_id]
     end
-    unless params[:account_type_id] == '0'
+    unless params[:account_type_id].empty?
       select_string += ' AND accounts.account_type_id = ?'
       query_params << params[:account_type_id]
     end
-    unless params[:account_owner_id] == '0'
+    unless params[:account_owner_id].empty?
       select_string += ' AND accounts.account_owner_id = ?'
       query_params << params[:account_owner_id]
     end
