764d509563c5052708ed70e1647926c7cded56fd
Eric Zetterbaum <ezetter@gmail.com>
2015-01-04 21:43:13 -0800

    Added ability to compare future values for two scenarios.

--- app/controllers/portfolio_analysis_controller.rb
+++ app/controllers/portfolio_analysis_controller.rb
@@ -2,7 +2,8 @@ class PortfolioAnalysisController < ApplicationController
   include Common
 
   def future_value
-    @portfolio_analysis = PortfolioAnalyser.new(params)
+    @portfolio_analysis_1 = PortfolioAnalyser.new(portfolio_analysis_params(1))
+    @portfolio_analysis_2 = PortfolioAnalyser.new(portfolio_analysis_params(2))
     if params[:view] == 'chart'
       render "future_value_chart"
     end
@@ -10,8 +11,12 @@ class PortfolioAnalysisController < ApplicationController
 
   private
 
-  def future_value_params
-    params.require(:account).permit(:name, :value, :date_opened, :account_type_id, :account_owner_id)
+  def portfolio_analysis_params(scenario_num)
+    attributes = {}
+    attributes[:rate] = params["rate_#{scenario_num}"]
+    attributes[:payment] = params["payment_#{scenario_num}"]
+    attributes[:periods] = params[:periods]
+    attributes
   end
 
 end
