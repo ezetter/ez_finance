d9738a723ba117b25005b53c2122c18a97442261
Eric Zetterbaum <ezetter@gmail.com>
2015-01-17 16:17:04 -0800

    Change admin nav item to dropdown.
    Delete admin view/controller/route since they're not used now.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :account_types
   resources :account_owners
   resources :account_history, :only => [:index]
-  resources :admin, :only => [:index]
 
   get 'analysis/future' => 'portfolio_analysis#future_value'
   get 'analysis/breakdown' => 'portfolio_analysis#breakdown'
